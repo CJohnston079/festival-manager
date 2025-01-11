3e46e61a845a9734d7bc3f590033fc7161e6caf9
Chris <chris79gp@outlook.com>
2025-01-11 18:24:46 +0000

    Feat: add `read_csv()` - `read_csv()` takes a path to a CSV file and returns the file as a list of dictionaries.

--- src/utils/read_csv.py
+++ src/utils/read_csv.py
@@ -1,2 +1,10 @@
+import csv
+
+
 def read_csv(file_path):
-    return
+    csv_data = []
+    with open(file_path, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            csv_data.append(row)
+    return csv_data
