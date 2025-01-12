47e27047fae69adbfc4652acacde8daf154fecfe
Chris <chris79gp@outlook.com>
2025-01-12 19:34:56 +0000

    Feat: add unit test for `add_entrants_to_lesson`

---
+++ src/add_entrants_to_lesson.py
@@ -0,0 +1,2 @@
+def add_entrants_to_lesson():
+    return
---
+++ test/test_add_entrants_to_lesson.py
@@ -0,0 +1,74 @@
+import pytest
+
+from src.add_entrants_to_lesson import add_entrants_to_lesson
+
+
+@pytest.mark.xfail
+class TestAddEntrantsToLesson:
+    @pytest.fixture
+    def test_data(self):
+        test_lesson_data = [
+            {
+                "Number": "001",
+                "Name": "PIANO BEGINNERS",
+                "Description": "Own choice.",
+                "Max Duration": "3",
+            },
+            {
+                "Number": "002",
+                "Name": "PIANO INTERMEDIATE",
+                "Description": "Own choice.",
+                "Max Duration": "4",
+            },
+            {
+                "Number": "003",
+                "Name": "PIANO ADVANCED",
+                "Description": "Own choice.",
+                "Max Duration": "5",
+            }
+        ]
+        test_entrant_data = [
+            {"Class Number": "001"},
+            {"Class Number": "001"},
+            {"Class Number": "001"},
+            {"Class Number": "001"},
+            {"Class Number": "002"},
+            {"Class Number": "002"},
+            {"Class Number": "002"},
+            {"Class Number": "003"}
+        ]
+
+        return test_lesson_data, test_entrant_data
+
+    def test_add_entrants_to_lesson(self, test_data, mocker):
+        lessons_data, entrants_data = test_data
+
+        mocker.patch(
+            "src.add_entrants_to_lesson.read_csv",
+            side_effect=lambda file_path: (
+                lessons_data if "lesson" in file_path else entrants_data
+            )
+        )
+        expected = [
+            {
+                "Number": "001",
+                "Name": "PIANO BEGINNERS",
+                "Max Duration": "3",
+                "Number of entries": 4
+            },
+            {
+                "Number": "002",
+                "Name": "PIANO INTERMEDIATE",
+                "Max Duration": "4",
+                "Number of entries": 3
+            },
+            {
+                "Number": "003",
+                "Name": "PIANO ADVANCED",
+                "Max Duration": "5",
+                "Number of entries": 1
+            }
+        ]
+        result = add_entrants_to_lesson("lessons.csv", "entrants.csv")
+
+        assert result == expected
